78223d6b57d99217a7e9758e74d0d3c3c0804cf1
Chrissou <nicolchristelle@gmail.com>
2018-05-16 15:39:10 +0200

    state initial et count pour counter

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -15,7 +15,37 @@ class App extends React.Component {
   /* Actions
   *
   */
-
+/*
+* state initial
+*/
+state = {
+  tasks: [
+    {
+      id: 1,
+      label: 'Faire une todo-list en JS',
+      done: true,
+      favorite: false,
+    },
+    {
+      id: 12,
+      label: 'Faire une todo-list en React',
+      done: false,
+      favorite: true,
+    },
+    {
+      id: 43,
+      label: 'Vivre paisiblement',
+      done: false,
+      favorite: false,
+    },
+    {
+      id: 5,
+      label: 'Coder Facebook',
+      done: false,
+      favorite: false,
+    },
+  ],
+}
  // fonction qui recup la valeur de l'imput
  handleSubmit = (evt) => {
    evt.preventDefault();
@@ -23,11 +53,17 @@ class App extends React.Component {
    console.log(input.value);
  }
  render() {
+   // vars pour recuperer les taches
+   const { tasks } = this.state;
+   // recup tache pas effectuées
+   const undoneTasks = tasks.filter(task => !task.done);
+   // on compte le nombre de tâches
+   const count = undoneTasks.length;
    return (
      <div id="todo">
        <Form onFormSubmit={this.handleSubmit} />
-       <Counter count={2} />
-       <Tasks tasks={[]} />
+       <Counter count={count} />
+       <Tasks tasks={tasks} />
      </div>
    );
  }
